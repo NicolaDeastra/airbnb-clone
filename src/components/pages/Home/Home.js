7eef33938bf48ff7c7a1c94d863caa89dbc46fd5
NicolaDonoastro <nicolaardiansya@gmail.com>
2020-08-31 14:42:36 +0700

    add navbar component

---
+++ src/components/pages/Home/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Banner from "../../Banner/Banner";
+
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Home component</h1>
+    </div>
+  );
+}
+
+export default Home;
